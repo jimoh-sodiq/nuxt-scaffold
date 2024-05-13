841569ce1304bf07bdf482d575f90c285925a4ac
jimoh-sodiq <jimohsodiq301@gmail.com>
2024-05-13 16:22:04 +0100

    show roadmap only on dev mode

--- src/components/global/GlobalNav.vue
+++ src/components/global/GlobalNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-slate-800 p-4 text-white flex items-center gap-x-4">
        <nav><nuxt-link to="/">Nuxt Scaffold</nuxt-link></nav>
-       <nuxt-link to="/roadmap" class="block">View Roadmap</nuxt-link>
+       <dev-only><nuxt-link to="/roadmap" class="block">View Roadmap</nuxt-link></dev-only>
     </div>
 </template>
