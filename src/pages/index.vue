bce30cc21e10d14431bb1bbc5db5f27c58c94c4b
jimoh-sodiq <jimohsodiq301@gmail.com>
2024-05-13 12:13:12 +0100

    generate app.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 // import { createFile } from "../index"
 
+type Module = {
+  name: string, version: string, dependency: boolean
+}
+
+const nuxtDirectories = [
+  "assets", "components", "composables", "layouts", "middleware", "pages", "plugins", "server", "utils"
+]
+
+// App State
+const selectedModules = ref<Array<Module>>([])
+const selectedDirectories = ref<Array<string>>(["public"])
+const srcDir = ref("src")
+const projectName = ref("nuxt-app")
+
+
 
 function generateNuxtConfig({ srcDir = ".", modules = [] }: {
   srcDir?: string, modules?: Array<string>
@@ -20,17 +35,14 @@ function generateNuxtConfig({ srcDir = ".", modules = [] }: {
 async function createProject(name: string, fileName: string, fileContent: string) {
   const dirHandle = await window.showDirectoryPicker();
 
-  //   await createDirectoryWithSingleFile(dirHandle, "server", "tsconfig.json", `{
-  //   "extends": "../.nuxt/tsconfig.server.json"
-  // }`)
+  await generateDirectories(dirHandle, selectedDirectories.value)
+  await generateAppDotVueFile(dirHandle, selectedDirectories.value.includes("pages"), selectedDirectories.value.includes("layouts"))
 
-  // await createFile(dirHandle, "nuxt.config.ts", generateNuxtConfig({ srcDir: 'src', modules: ["@nuxt/image", "@vueuse/nuxt"] }))
-  await generateDirectories(dirHandle, nuxtDirectories)
+  // use the same root handle these
+  await generateGitignoreFile(dirHandle)
+  await generateTailwindConfigFile(dirHandle)
+  await generateTSConfigFile(dirHandle)
 }
-
-const nuxtDirectories = [
-  "assets", "components", "composables", "layouts", "middleware", "plugins", "server", "utils"
-]
 
 const nuxtModules = [
 
@@ -56,6 +68,97 @@ async function generateServerDirectory(handle: any) {
   `)
 }
 
+async function generateReadmeFile(roothandle: any) {
+  // await createFile(roothandle, "README.md",)
+}
+
+async function generateGitignoreFile(rootHandle: any) {
+  await createFile(rootHandle, ".gitignore", `# Nuxt dev/build outputs
+.output
+.data
+.nuxt
+.nitro
+.cache
+dist
+
+# Node dependencies
+node_modules
+
+# Logs
+logs
+*.log
+
+# Misc
+.DS_Store
+.fleet
+.idea
+
+# Local env files
+.env
+.env.*
+!.env.example
+
+  `)
+}
+
+async function generateTailwindConfigFile(rootHandle: any) {
+  await createFile(rootHandle, "tailwind.config.ts", `import type { Config } from "tailwindcss"\n
+export default <Partial<Config>>{
+	content: [
+	],
+	theme: {
+		extend: {},
+	},
+}
+`)
+}
+
+async function generateTSConfigFile(rootHandle: any) {
+  await createFile(rootHandle, "tsconfig.json", `{
+  // https://nuxt.com/docs/guide/concepts/typescript
+  "extends": "./.nuxt/tsconfig.json"
+}
+`)
+}
+
+async function generatePagesDirectory(handle: any) {
+  await createDirectoryWithSingleFile(handle, "pages", "index.vue", `<template>
+  <div>
+    Nuxt Application Scaffold
+  </div>
+</template>
+`)
+}
+
+async function generateAppDotVueFile(handle: any, pagesExist?: boolean, layoutExists?: boolean) {
+  let content = `<template>
+  <h1>Hello World!</h1>
+</template>`
+
+  const pageContent = `<template>
+  <div>
+    <NuxtPage/>
+  </div>
+</template>`
+
+  const layoutContent = `<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/> 
+    </NuxtLayout>
+  </div>
+</template>`
+
+  if (pagesExist) {
+    content = pageContent
+  }
+  if (layoutExists) {
+    content = layoutContent
+  }
+
+  await createFile(handle, "app.vue", content)
+}
+
 async function generateDirectories(handle: any, directories: Array<string>) {
   directories.forEach(async (directory: string) => {
     if (directory == 'layouts') {
@@ -64,12 +167,19 @@ async function generateDirectories(handle: any, directories: Array<string>) {
     if (directory == 'server') {
       return await generateServerDirectory(handle)
     }
+    if (directory == "pages") {
+      return await generatePagesDirectory(handle)
+    }
     await createDirectory(handle, directory)
   })
 }
 
 
-async function scaffoldApp() {
+
+async function scaffoldApp(rootHandle: any) {
+  // await generateGitignoreFile()
+  // await generateReadmeFile()
+  // await generateDirectories()
 
 }
 
@@ -93,8 +203,8 @@ async function scaffoldApp() {
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         <GlobalInfoCard title="Application settings">
           <div class="grid md:flex flex-wrap lg:flex-nowrap gap-4">
-            <GlobalInput label="Project Name" placeholder="e.g nuxt-app" />
-            <GlobalInput label="Src Diriectory" placeholder="e.g src, source, project" />
+            <GlobalInput label="Project Name" placeholder="e.g nuxt-app" v-model="projectName" />
+            <GlobalInput label="Src Diriectory" placeholder="e.g src, source, project" v-model="srcDir" />
           </div>
         </GlobalInfoCard>
         <GlobalInfoCard title="Nuxt official Modules">
@@ -115,9 +225,9 @@ async function scaffoldApp() {
         </GlobalInfoCard>
         <GlobalInfoCard title="Directories">
           <div class="flex flex-wrap gap-4">
-            <GlobalCheckbox label="pages" disabled />
-            <GlobalCheckbox label="public" disabled />
-            <GlobalCheckbox :label="directory" :key="directory" v-for="directory in nuxtDirectories" />
+            <GlobalCheckbox v-model="selectedDirectories" value="public" label="public" disabled />
+            <GlobalCheckbox v-model="selectedDirectories" :value="directory" :label="directory" :key="directory"
+              v-for="directory in nuxtDirectories" />
           </div>
         </GlobalInfoCard>
       </div>
